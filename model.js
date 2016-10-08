d34405d2485095c6e27a6b937bdeefd3bee21946
Jon White <jxxwhite@gmail.com>
2016-10-08 14:54:54 +0100

    Added money.

--- model.js
+++ model.js
@@ -1,4 +1,24 @@
 "use strict";
 
+function assert(condition, message)
+{
+    if (!condition)
+        alert(message ? 'Assertion failed: ' + message : 'Assertion failed');
+}
+
 var Model = {}
 
+Model._money = 100;
+
+Model.getMoney = function() { return this._money; }
+
+Model.spendMoney = function(amount) 
+{
+    assert(amount >= 0 && this._money >= amount);
+    this._money -= amount;
+}
+
+Model.addMoney = function (amount) {
+    assert(amount >= 0);
+    this._money += amount;
+}
