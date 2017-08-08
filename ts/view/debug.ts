abf1d9278d16d92367a8ae0afd8c8f66199f2207
Jon White <jxxwhite@gmail.com>
2017-08-08 13:09:56 +0100

    Don't auto-hide debug page

--- ts/view/debug.ts
+++ ts/view/debug.ts
@@ -31,7 +31,6 @@ namespace View
 				Model.state.addMoney(Data.Animals.Types[tag].cost);
 				Model.state.buyAnimal(tag);
 			}
-			Page.hideCurrent();
 		}
 
 		onBuyAllPeople = () =>
@@ -41,7 +40,6 @@ namespace View
 				Model.state.addMoney(Data.People.Types[tag].cost);
 				Model.state.buyPerson(tag);
 			}
-			Page.hideCurrent();
 		}
 
 		onBuyAllBuildings = () =>
@@ -57,22 +55,17 @@ namespace View
 					Model.state.buildings.buyUpgrade(tag);
 				}
 			}
-			Page.hideCurrent();
 		}
 
 		onHeal = () =>
 		{
 			for (let id in Model.state.team.fighters)
 				Model.state.team.fighters[id].resetHealth();
-
-			Page.hideCurrent();
 		}
 
 		onAddMoney = () =>
 		{
 			Model.state.addMoney(100);
-
-			Page.hideCurrent();
 		}
 	}
 }
