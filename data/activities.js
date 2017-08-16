3b3ea23441e9e9bc99b6b687b580c4bcf0e45283
Jon White <jxxwhite@gmail.com>
2017-08-16 14:59:34 +0100

    Added healing

--- data/activities.js
+++ data/activities.js
@@ -10,6 +10,7 @@ Data.Activities.Types = {
 //	tag												name					job		human	animal	free_work
 	'idle':				new Data.Activities.Type(	'Idle',					false,	true,	true,	0	),
 	'build':			new Data.Activities.Type(	'Building',				true,	true,	false,	0.1	),
+	'heal':				new Data.Activities.Type(	'Healing',				false,	true,	false,	0	),
 	'train:attack':		new Data.Activities.Type(	'Training: attack',		false,	true,	false,	0	),
 	'train:evade':		new Data.Activities.Type(	'Training: evade',		false,	true,	false,	0	),
 };
